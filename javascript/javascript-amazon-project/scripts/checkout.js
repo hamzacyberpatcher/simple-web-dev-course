3f4b20296f1ed740cba6fc61d85e73da2ee5f50f
hamzacyberpatcher <hamzaoffuture@gmail.com>
2026-02-22 23:04:11 +0500

    delete cart items

--- javascript/javascript-amazon-project/scripts/checkout.js
+++ javascript/javascript-amazon-project/scripts/checkout.js
@@ -1,19 +1,21 @@
 import * as CartModule from '../data/cart.js'
 import * as ProductsModule from '../data/products.js'
 
-let orderSummaryHtml = '';
-
 function updateCheckoutQuantity() {
     document.querySelector('.js-checkout-quantity').innerHTML = `${CartModule.cartQuantity()} items`;
+    document.querySelector('.js-payment-order-quantity').innerHTML = `Items (${CartModule.cartQuantity()}):`
 }
 
-CartModule.cart.forEach(cartItem => {
-    const {productId, quantity} = cartItem;
+function renderCheckoutPage() {
+    let orderSummaryHtml = '';
+
+    CartModule.cart.forEach(cartItem => {
+    const { productId, quantity } = cartItem;
 
     const cartProduct = ProductsModule.findProduct(productId);
 
     const orderSummary = `
-        <div class="cart-item-container">
+        <div class="cart-item-container js-cart-item-container-${productId}">
                 <div class="delivery-date">
                 Delivery date: Tuesday, June 21
                 </div>
@@ -36,7 +38,7 @@ CartModule.cart.forEach(cartItem => {
                     <span class="update-quantity-link link-primary">
                         Update
                     </span>
-                    <span class="delete-quantity-link link-primary">
+                    <span class="delete-quantity-link link-primary js-link-delete" data-product-id=${productId}>
                         Delete
                     </span>
                     </div>
@@ -92,8 +94,30 @@ CartModule.cart.forEach(cartItem => {
     `;
 
     orderSummaryHtml += orderSummary;
-});
+    });
 
-document.querySelector('.js-order-summary').innerHTML = orderSummaryHtml;
+    document.querySelector('.js-order-summary').innerHTML = orderSummaryHtml;
 
-updateCheckoutQuantity();
+    document.querySelectorAll('.js-link-delete').forEach(elem => {
+    elem.addEventListener('click', () => {
+            const {productId} = elem.dataset;
+
+            let index = CartModule.cart.findIndex(item => item.productId === productId);
+
+            if (index != -1) {
+                CartModule.cart.splice(index, 1);
+            }
+
+            localStorage.setItem('cart', JSON.stringify(CartModule.cart));
+
+            document.querySelector(`.js-cart-item-container-${productId}`).remove();
+            updateCheckoutQuantity();
+        });
+    })
+
+
+    updateCheckoutQuantity();
+}
+
+
+renderCheckoutPage();
