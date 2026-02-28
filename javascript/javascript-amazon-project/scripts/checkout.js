0062c00a6932c774f72bafc53dd53171f26fe72c
hamzacyberpatcher <hamzaoffuture@gmail.com>
2026-02-28 22:29:45 +0500

    order wali thori si bullshit

--- javascript/javascript-amazon-project/scripts/checkout.js
+++ javascript/javascript-amazon-project/scripts/checkout.js
@@ -106,13 +106,29 @@ function renderPaymentSummary() {
             <div class="payment-summary-money">$${(net / 100).toFixed(2)}</div>
           </div>
 
-          <button class="place-order-button button-primary">
+          <button class="place-order-button button-primary js-place-order">
             Place your order
           </button>
         </div>
     `;
 
     document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHtml;
+    document.querySelector('.js-place-order')
+    .addEventListener('click', async () => {
+        const response = await fetch('https://supersimplebackend.dev/orders', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                cart: CartModule.cart
+            })
+        });
+
+        const orderResponse = await response.json();
+
+        console.log(orderResponse);
+    });
 }
 
 function renderCheckoutPage() {
