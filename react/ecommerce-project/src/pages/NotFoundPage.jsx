c0417478791e7e44394ef53dca96805098fe8435
hamzacyberpatcher <hamzaoffuture@gmail.com>
2026-03-06 22:13:43 +0500

    added a not found page

---
+++ react/ecommerce-project/src/pages/NotFoundPage.jsx
@@ -0,0 +1,11 @@
+import './NotFoundPage.css'
+
+export function NotFoundPage() {
+    return (
+        <>
+            <div className="not-found-container">
+                <p>404 Not Found</p>
+            </div>
+        </>
+    );
+}
