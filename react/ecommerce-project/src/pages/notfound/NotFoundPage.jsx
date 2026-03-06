d3ea2a86542e412565b1ff5320ade6cdb050c4c7
hamzacyberpatcher <hamzaoffuture@gmail.com>
2026-03-07 01:31:57 +0500

    passed header into not found page

--- react/ecommerce-project/src/pages/notfound/NotFoundPage.jsx
+++ react/ecommerce-project/src/pages/notfound/NotFoundPage.jsx
@@ -1,8 +1,10 @@
 import './NotFoundPage.css'
+import { Header } from '../../components/Header';
 
-export function NotFoundPage() {
+export function NotFoundPage({ cart }) {
     return (
         <>
+            <Header cart={cart} />
             <div className="not-found-container">
                 <p>404 Not Found</p>
             </div>
