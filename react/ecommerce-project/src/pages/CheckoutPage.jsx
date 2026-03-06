9176dac583c9db01303e8444c8d55a0fbd482d63
hamzacyberpatcher <hamzaoffuture@gmail.com>
2026-03-06 21:26:40 +0500

    added checkout page header component

--- react/ecommerce-project/src/pages/CheckoutPage.jsx
+++ react/ecommerce-project/src/pages/CheckoutPage.jsx
@@ -1,4 +1,4 @@
-import './checkout-header.css'
+import { CheckoutPageHeader } from './CheckoutPage/CheckoutPageHeader';
 import './CheckoutPage.css'
 import { Link } from 'react-router';
 
@@ -6,25 +6,8 @@ export function CheckOutPage() {
     return (
         <>
             <title>Checkout</title>
-            <div className="checkout-header">
-                <div className="header-content">
-                    <div className="checkout-header-left-section">
-                        <Link to="/">
-                            <img className="logo" src="images/logo.png" />
-                            <img className="mobile-logo" src="images/mobile-logo.png" />
-                        </Link>
-                    </div>
 
-                    <div className="checkout-header-middle-section">
-                        Checkout (<Link className="return-to-home-link"
-                            to="/">3 items</Link>)
-                    </div>
-
-                    <div className="checkout-header-right-section">
-                        <img src="images/icons/checkout-lock-icon.png" />
-                    </div>
-                </div>
-            </div>
+            <CheckoutPageHeader />
 
             <div className="checkout-page">
                 <div className="page-title">Review your order</div>
