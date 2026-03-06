6d63b6e8479694f3cfb21c65c5e2ab87d6e8d63f
hamzacyberpatcher <hamzaoffuture@gmail.com>
2026-03-06 22:52:39 +0500

    loaded products and cart data from backend

--- react/ecommerce-project/src/pages/HomePage.jsx
+++ react/ecommerce-project/src/pages/HomePage.jsx
@@ -1,15 +1,30 @@
 import { Header } from '../components/Header';
 import './HomePage.css'
 import HomeFavicon from '../assets/home-favicon.png'
-import { products } from './data/products';
+import axios from 'axios'
+import { useState, useEffect } from 'react';
 
 export function HomePage() {
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/api/products').then(response => {
+            setProducts(response.data);
+        });
+
+        axios.get('http://localhost:3000/api/cart-items').then(response => {
+            setCart(response.data);
+        });
+    }, []);
+
+
     return (
         <>
             <title>Ecommerce Project</title>
             <link rel="icon" href={HomeFavicon} />
 
-            <Header />
+            <Header cart={cart} />
 
             <div className="home-page">
                 <div className="products-grid">
